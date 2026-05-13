62ba71adb2f388f45597392e53b37cd868f24db1
Douglas Rosso <douglasrosso10@gmail.com>
2026-05-12 23:23:11 -0300

    test rule

--- src/Controllers/UsersController.cs
+++ src/Controllers/UsersController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using nebula.api.src.DTOs;
 using nebula.api.src.Services;
